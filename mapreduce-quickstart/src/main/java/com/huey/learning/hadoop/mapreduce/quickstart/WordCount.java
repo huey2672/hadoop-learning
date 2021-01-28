a6bba71509b98611d2f6d0bb5fc095c67f532a90
huey <huey2672@163.com>
2021-01-29 01:43:42 +0800

    hadoop

---
+++ mapreduce-quickstart/src/main/java/com/huey/learning/hadoop/mapreduce/quickstart/WordCount.java
@@ -0,0 +1,63 @@
+package com.huey.learning.hadoop.mapreduce.quickstart;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * @author huey
+ */
+public class WordCount {
+
+    public static class TokenizerMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+        private final static LongWritable ONE = new LongWritable(1);
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                Text word = new Text(itr.nextToken());
+                context.write(word, ONE);
+            }
+        }
+
+    }
+
+    public static class SumReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long sum = 0;
+            for (LongWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new LongWritable(sum));
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Word Count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(SumReducer.class);
+        job.setReducerClass(SumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
